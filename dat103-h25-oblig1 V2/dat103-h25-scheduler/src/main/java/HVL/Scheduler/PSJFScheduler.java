73cfa85b6c95f1ea4955015ca74c307e2dbcfbae
186480 <186480@stud.hvl.no>
2025-10-10 16:28:36 +0200

    op

--- dat103-h25-oblig1 V2/dat103-h25-scheduler/src/main/java/HVL/Scheduler/PSJFScheduler.java
+++ dat103-h25-oblig1 V2/dat103-h25-scheduler/src/main/java/HVL/Scheduler/PSJFScheduler.java
@@ -1,6 +1,4 @@
 package HVL.Scheduler;
-
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
--- dat103-h25-oblig1 V2/dat103-h25-scheduler/src/test/java/HVL/Scheduler/SimulationTest.java
+++ dat103-h25-oblig1 V2/dat103-h25-scheduler/src/test/java/HVL/Scheduler/SimulationTest.java
@@ -14,7 +14,7 @@ public class SimulationTest {
 
     Map<Integer, List<Task>> arrivals;
     Simulation simulation;
-    private Integer test=1;  // DO NOT change this for Subtasks 1 and 2(a)
+    private Integer test=2;  // DO NOT change this for Subtasks 1 and 2(a)
                              // ONLY change the value from 1 to 2 for Subtask 2(b).
 
     @BeforeEach
@@ -24,7 +24,7 @@ public class SimulationTest {
 	//Test 1
 	case 1:  
 	        arrivals = Map.ofEntries(
-        Map.entry(0, List.of(
+                Map.entry(0, List.of(
                         simulation.makeTask(5),  
                         simulation.makeTask(3),  
                         simulation.makeTask(1))),
@@ -34,7 +34,7 @@ public class SimulationTest {
 		Map.entry(6, List.of(
                         simulation.makeTask(6),  
                         simulation.makeTask(4))),
-        Map.entry(10, List.of(  
+                Map.entry(10, List.of(  
                         simulation.makeTask(2))),
 		Map.entry(16, List.of(
                         simulation.makeTask(1),  
@@ -43,10 +43,10 @@ public class SimulationTest {
 	//Test 2		
 	case 2: 
 	        arrivals = Map.ofEntries(
-        Map.entry(0, List.of(
-                        simulation.makeTask(1),  
-                        simulation.makeTask(5),  
-                        simulation.makeTask(3))),
+                Map.entry(0, List.of(
+                        simulation.makeTask(1), //T1 
+                        simulation.makeTask(5),  //T2
+                        simulation.makeTask(3))), //T3
 		Map.entry(4, List.of(
                         simulation.makeTask(2),  
                         simulation.makeTask(4),  
@@ -73,45 +73,44 @@ public class SimulationTest {
             simulation.step();
             var state = "T=%d %s".formatted(simulation.time(), rrScheduler.view());
             simulation.clocktick();
-            System.out.println(state);
             return state;
 	    }).limit(31).collect(Collectors.toList());  //31 is the total execution time
 
         // Subtask 1: Write out expected view for 31 steps of Round Robin scheduling
 	// Assert Test 1
         assertThat(steps, contains(
-        	    "T=0 Scheduled: T1 Ready: T2, T3",
-        	    "T=1 Scheduled: T1 Ready: T2, T3",
-        	    "T=2 Scheduled: T1 Ready: T2, T3, T4, T5",
-        	    "T=3 Scheduled: T1 Ready: T2, T3, T4, T5",
-        	    "T=4 Scheduled: T2 Ready: T3, T4, T5, T1",
-        	    "T=5 Scheduled: T2 Ready: T3, T4, T5, T1",
-        	    "T=6 Scheduled: T2 Ready: T3, T4, T5, T1, T6, T7",
-        	    "T=7 Scheduled: T3 Ready: T4, T5, T1, T6, T7",
-        	    "T=8 Scheduled: T4 Ready: T5, T1, T6, T7",
-        	    "T=9 Scheduled: T5 Ready: T1, T6, T7",
-        	    "T=10 Scheduled: T5 Ready: T1, T6, T7, T8",
-        	    "T=11 Scheduled: T5 Ready: T1, T6, T7, T8",
-        	    "T=12 Scheduled: T5 Ready: T1, T6, T7, T8",
-        	    "T=13 Scheduled: T1 Ready: T6, T7, T8",
-        	    "T=14 Scheduled: T6 Ready: T7, T8",
-        	    "T=15 Scheduled: T6 Ready: T7, T8",
-        	    "T=16 Scheduled: T6 Ready: T7, T8, T9, T10",
-        	    "T=17 Scheduled: T6 Ready: T7, T8, T9, T10",
-        	    "T=18 Scheduled: T7 Ready: T8, T9, T10, T6",
-        	    "T=19 Scheduled: T7 Ready: T8, T9, T10, T6",
-        	    "T=20 Scheduled: T7 Ready: T8, T9, T10, T6",
-        	    "T=21 Scheduled: T7 Ready: T8, T9, T10, T6",
-        	    "T=22 Scheduled: T8 Ready: T9, T10, T6",
-        	    "T=23 Scheduled: T8 Ready: T9, T10, T6",
-        	    "T=24 Scheduled: T9 Ready: T10, T6",
-        	    "T=25 Scheduled: T10 Ready: T6",
-        	    "T=26 Scheduled: T10 Ready: T6",
-        	    "T=27 Scheduled: T10 Ready: T6",
-        	    "T=28 Scheduled: T6 Ready: ",
-        	    "T=29 Scheduled: T6 Ready: ",
-        	    "T=30 Scheduled: Ready: "
-        	));
+                "T=0 Scheduled: T1 Ready: T2, T3",
+                "T=1 Scheduled: T1 Ready: T2, T3",
+                "T=2 Scheduled: T1 Ready: T2, T3, T4, T5",
+                "T=3 Scheduled: T1 Ready: T2, T3, T4, T5",
+                "T=4 Scheduled: T2 Ready: T3, T4, T5, T1",
+                "T=5 Scheduled: T2 Ready: T3, T4, T5, T1",
+                "T=6 Scheduled: T2 Ready: T3, T4, T5, T1, T6, T7",
+                "T=7 Scheduled: T3 Ready: T4, T5, T1, T6, T7",
+                "T=8 Scheduled: T4 Ready: T5, T1, T6, T7",
+                "T=9 Scheduled: T5 Ready: T1, T6, T7",
+                "T=10 Scheduled: T5 Ready: T1, T6, T7, T8",
+                "T=11 Scheduled: T5 Ready: T1, T6, T7, T8",
+                "T=12 Scheduled: T5 Ready: T1, T6, T7, T8",
+                "T=13 Scheduled: T1 Ready: T6, T7, T8",
+                "T=14 Scheduled: T6 Ready: T7, T8",
+                "T=15 Scheduled: T6 Ready: T7, T8",
+                "T=16 Scheduled: T6 Ready: T7, T8, T9, T10",
+                "T=17 Scheduled: T6 Ready: T7, T8, T9, T10",
+                "T=18 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=19 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=20 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=21 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=22 Scheduled: T8 Ready: T9, T10, T6",
+                "T=23 Scheduled: T8 Ready: T9, T10, T6",
+                "T=24 Scheduled: T9 Ready: T10, T6",
+                "T=25 Scheduled: T10 Ready: T6",
+                "T=26 Scheduled: T10 Ready: T6",
+                "T=27 Scheduled: T10 Ready: T6",
+                "T=28 Scheduled: T6 Ready: ",
+                "T=29 Scheduled: T6 Ready: ",
+                "T=30 Scheduled: Ready: "
+            ));
 	break;
 	
 	case 2: break;
@@ -133,9 +132,9 @@ public class SimulationTest {
             return state;		    
 	    }).limit(31).collect(Collectors.toList());
 	
-       assertThat(steps1,contains(
-"T=0 Scheduled: T3 Ready: T2, T1",
-    	"T=1 Scheduled: T2 Ready: T1",
+        assertThat(steps1, contains(
+        "T=0 Scheduled: T3 Ready: T2, T1",
+        "T=1 Scheduled: T2 Ready: T1",
 		"T=2 Scheduled: T4 Ready: T2, T5, T1",
 		"T=3 Scheduled: T2 Ready: T5, T1",
 		"T=4 Scheduled: T2 Ready: T5, T1",
@@ -164,7 +163,7 @@ public class SimulationTest {
 		"T=27 Scheduled: T6 Ready: ",
 		"T=28 Scheduled: T6 Ready: ",
 		"T=29 Scheduled: T6 Ready: ",
-                "T=30 Scheduled: Ready: "	
+        "T=30 Scheduled: Ready: "	
         ));
 	break;
 
